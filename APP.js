6776c9771eddefa1f6e861d17e5900f6f5e9730d
lammy-prime <54742669+lammy-prime@users.noreply.github.com>
2019-09-03 19:24:08 +0100

    Initial commit

---
+++ APP.js
@@ -0,0 +1,11 @@
+//person constructor
+//function person() {
+  //this.name = 'brad';
+}
+//const brad = new person();
+//console.log(brad);
+//PRIMITIVE
+//STRING
+const name = 'lammy_prime';
+
+console.log(typeof name);
